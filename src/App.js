b67cec64c1f2cb1acff0d11e9bb998a50250c57a
efmorales <54375116+efmorales@users.noreply.github.com>
2023-06-02 20:42:02 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import clothing from './clothing';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Outfit planner</h1>
+    </div>
+  );
+}
+
+export default App;
